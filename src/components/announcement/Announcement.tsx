8482908b7721cfc2ce28c2e15f017302ed312ecf
Ali Nazery <ali.nazery@example.com>
2024-09-11 08:07:31 +0430

    announcemnent updated

--- src/components/announcement/Announcement.tsx
+++ src/components/announcement/Announcement.tsx
@@ -1,5 +1,17 @@
+"use client"
+
+import { useState } from "react";
+import { CloseSvg } from "../Common/Icons/Announcement";
 
 export default function Announcement() {
+  const [isVisible, setIsVisible]=useState(true)
+
+  const handleClick = () => {
+    setIsVisible(false)
+  }
+
+  if (!isVisible) return null;
+
   return (
     <div className="annoucement relative z-30 bg-black bg-opacity-100 text-white py-2">
       <div className="sm:mx-auto px-4 md:text-15px xs:text-14px leading-6 xs:font-thin lg:px-10">
@@ -18,14 +30,9 @@ export default function Announcement() {
             className="flex h-5 min-w-5 items-center justify-center rounded-full border-2 border-white text-white opacity-80"
             aria-label="close toast button"
             type="button"
+            onClick={handleClick}
           >
-            <svg
-              className="w-3 h-3 md:h-4 md:w-4 text-current fill-current "
-              viewBox="0 0 512 512"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M278.6 256l68.2-68.2c6.2-6.2 6.2-16.4 0-22.6-6.2-6.2-16.4-6.2-22.6 0L256 233.4l-68.2-68.2c-6.2-6.2-16.4-6.2-22.6 0-3.1 3.1-4.7 7.2-4.7 11.3 0 4.1 1.6 8.2 4.7 11.3l68.2 68.2-68.2 68.2c-3.1 3.1-4.7 7.2-4.7 11.3 0 4.1 1.6 8.2 4.7 11.3 6.2 6.2 16.4 6.2 22.6 0l68.2-68.2 68.2 68.2c6.2 6.2 16.4 6.2 22.6 0 6.2-6.2 6.2-16.4 0-22.6L278.6 256z" />
-            </svg>
+            <CloseSvg/>
           </button>
         </div>
       </div>
